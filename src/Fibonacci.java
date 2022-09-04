88a8a3c1938dfa9a22353f854fcd787946949178
Ilia Isaev <isaev_ilya@bk.ru>
2022-09-04 17:35:37 +0300

    init commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+//        System.out.println(fibNaive(3));
+//        System.out.println(fibNaive(5));
+//        System.out.println(fibNaive(10));
+        int n = 100;
+        long[] mem = new long[n + 1];
+
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaive(n, mem));
+    }
+
+    // наивным, медленным, очевидным
+    private static long fibNaive(int n, long[] mem) {
+        if (mem[n] != -1) {
+            return mem[n];
+        }
+        if (n <= 1) {
+            return n;
+        }
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
+
+        return result;
+    }
+
+    private static long fibEffictive(int n) {
+        long[] arr = new long[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i = 2; i <= n; i ++) {
+            arr[i] = arr[i - 1]  + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
